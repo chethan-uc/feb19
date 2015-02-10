577e653b03f2d38be9b5b7a276367e560718256e
Abhilash Joseph C <abhilash@mangaloreinfotech.in>
2015-02-10 13:08:27 +0530

    created View

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
+from main.views import ExtractorList
 
 urlpatterns = patterns('main.views',
-
+	url(r'list', ExtractorList.as_view(), name='extractor_list')
 )
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView,CreateView
+from main.models import Extractor
 
-
+class ExtractorList(ListView):
+    model = Extractor
